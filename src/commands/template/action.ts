57e969dc4f8f9279a79dd2ce54caa1b21b74f891
Sunny Singh <me@sunnysingh.io>
2023-05-14 11:27:33 -0400

    Update default template

--- src/commands/template/action.ts
+++ src/commands/template/action.ts
@@ -53,6 +53,6 @@ import { defineTemplate } from "tsnew";
 
 export default defineTemplate((context) => ({
   path: \`${name}/\${context.input.name}.ts\`,
-  content: \`// Hello \${context.input.name}!\`,
+  content: \`// This is a starter file for the \${context.input.name} ${name}!\`,
 }));
 `.trim();
