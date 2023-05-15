d1845b60f24906ce3480d1bfb334171ed3821c6c
Sunny Singh <me@sunnysingh.io>
2023-05-14 21:59:19 -0400

    Delete tsnew dir

--- .tsnew/templates/blah/default.template.ts
+++
@@ -1,6 +0,0 @@
-import { defineTemplate } from "tsnew";
-
-export default defineTemplate((context) => ({
-  path: `blah/${context.input.name}.ts`,
-  content: `// This is a starter file for the ${context.input.name} blah!`,
-}));
--- .tsnew/templates/blah2/default.template.ts
+++
@@ -1,6 +0,0 @@
-import { defineTemplate } from "tsnew";
-
-export default defineTemplate((context) => ({
-  path: `blah2/${context.input.name}.ts`,
-  content: `// This is a starter file for the ${context.input.name} blah2!`,
-}));
