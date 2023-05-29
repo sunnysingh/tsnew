431dac1a93b209a6476d81545f9fecb5b38461f2
Sunny Singh <me@sunnysingh.io>
2023-05-28 21:57:48 -0400

    Add docs

--- site/theme.config.jsx
+++ site/theme.config.jsx
@@ -1,6 +1,54 @@
+import { useConfig } from 'nextra-theme-docs';
+
+import { Logo } from './components/Logo';
+
 export default {
-  logo: <span>tsnew</span>,
+  primaryHue: 175,
+  logo: <Logo size={130} />,
+  docsRepositoryBase: 'https://github.com/sunnysingh/tsnew/blob/main/site',
   project: {
     link: 'https://github.com/sunnysingh/tsnew',
+  },
+  feedback: {
+    content: null,
+  },
+  head: () => {
+    const { frontMatter } = useConfig();
+
+    return (
+      <>
+        <title>
+          {frontMatter.title ? `${frontMatter.title} | TSNew` : 'TSNew'}
+        </title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </>
+    );
+  },
+  footer: {
+    text: (
+      <span>
+        © {new Date().getFullYear()}{' '}
+        <a href="https://nextra.site" target="_blank">
+          Sunny Singh
+        </a>
+      </span>
+    ),
   },
 };
